1a2182032e7b11f79e07b4391827dc19ee28e01c
Surma <surma@surma.link>
2018-04-05 14:40:24 +0100

    Avbilities list!

--- monkeypatches/portshim.c
+++ monkeypatches/portshim.c
@@ -1,4 +1,4 @@
-// #include "emscripten.h"
+#include "emscripten.h"
 #include <gphoto2/gphoto2-abilities-list.h>
 #include <gphoto2/gphoto2-camera.h>
 #include <gphoto2/gphoto2-port-result.h>
@@ -40,4 +40,19 @@ void fill_the_list(CameraAbilitiesList *list) {
     strcpy (list->abilities[x].id, text.text);
     strcpy (list->abilities[x].library, "ptp2");
   }
+}
+
+EMSCRIPTEN_KEEPALIVE
+CameraAbility* malloc_camera_ability() {
+  return malloc(sizeof(CameraAbility));
+}
+
+EMSCRIPTEN_KEEPALIVE
+int camera_ability_get_vendor(CameraAbility* ca) {
+  return ca->usb_vendor;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int camera_ability_get_product(CameraAbility* ca) {
+  return ca->usb_product;
 }
